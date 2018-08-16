d110f30a2d150da356b0e5fd0b2e2056b077c45e
Regulyarniy Nikolay <regularniy@gmail.com>
2018-08-16 15:50:37 +1000

    Add the ability to create a post.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,62 @@
 <template>
-<div>
-  dash
+<div id="dashboard">
+
+  <section>
+    <div class="col1">
+      <div class="profile">
+        <h5>{{ userProfile.name }}</h5>
+        <p>{{ userProfile.title }}</p>
+        <div class="create-post">
+          <p>create a post</p>
+          <form @submit.prevent>
+            <textarea v-model.trim="post.content"></textarea>
+            <button class="button" @click="createPost" :disabled="post.content == ''">post</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col2">
+      <div>
+        <p class="no-results">There are currently no posts</p>
+      </div>
+    </div>
+  </section>
+
 </div>
 </template>
 
 <script>
-export default {
+import {
+  mapState
+} from 'vuex'
+const fb = require('../firebaseConfig.js')
 
+export default {
+  data() {
+    return {
+      post: {
+        content: ''
+      }
+    }
+  },
+  computed: {
+    ...mapState(['userProfile', 'currentUser'])
+  },
+  methods: {
+    createPost() {
+      fb.postsCollection.add({
+        createdOn: new Date(),
+        content: this.post.content,
+        userId: this.currentUser.uid,
+        userName: this.userProfile.name,
+        comments: 0,
+        likes: 0
+      }).then(ref => {
+        this.post.content = ''
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
